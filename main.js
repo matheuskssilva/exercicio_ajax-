5e45017f1d2d6110f6565852103f29c6df5c5bee
Matheus Silva <matheuskauan2709@gmail.com>
2023-10-20 18:10:23 -0300

    adicionando arquivos do exercicio ajax

---
+++ main.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const public_repos = document.getElementById('repositorio')
+    const following = document.getElementById('seguidores')
+    const followers = document.getElementById('seguindo')
+    const nome = document.getElementById('nome')
+    const nomeUsuario = document.getElementById('nomeUsuario')
+    const avatar_url = document.getElementById('avatar')
+    const url = document.getElementById('link')
+
+
+    const endPoint = `https://api.github.com/users/ogiansouza`
+    console.log(endPoint)
+    fetch(endPoint)
+        .then(function(resposta) {
+            return resposta.json()
+
+        })
+        .then(function(json) {
+            public_repos.innerText = json.public_repos
+            following.innerText = json.following
+            followers.innerText = json.followers
+            nome.innerText = json.name
+            nomeUsuario.innerText = json.login
+            avatar_url.src = json.avatar_url
+            url.href = json.html_url
+        })
+
+})
